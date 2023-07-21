1b7435e2ed2c1aa954fdeab096c721664d16e8b7
Zackary Hughes <zackary.k.hughes@gmail.com>
2023-07-21 08:51:06 -0500

    Finished delete_exercises.sql

---
+++ delete_exercises.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+
+SELECT name, release_date FROM albums WHERE release_date > 1991;
+
+DELETE FROM albums WHERE release_date > 1991;
+
+SELECT name, genre FROM albums WHERE genre = 'Disco';
+
+DELETE FROM albums WHERE genre = 'Disco';
+
+SELECT name FROM albums WHERE artist = 'Whitney Houston / Various artists';
+
+DELETE FROM albums WHERE artist = 'Whitney Houston / Various artists';
